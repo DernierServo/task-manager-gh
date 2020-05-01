bc983955612faa5523fd99528f56a3a1556b61b4
dservo <arrasco.cs@gmail.com>
2020-05-01 15:19:35 -0500

    Adding templates html files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 app = Flask(__name__)
 
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
 	user_ip = request.remote_addr
 	response = make_response(redirect('/hello'))
-	response.set_cookie('usr_ip', user_ip)
+	response.set_cookie('user_ip', user_ip)
 
 	return response
 
@@ -14,8 +14,7 @@ def index():
 @app.route('/hello')
 def hello():
 	user_ip = request.cookies.get('user_ip')
-
-	return f'hello world from DServo labs, your IP is {user_ip}'
+	return render_template('hello.html', user_ip = user_ip)
 
 
 if __name__ == '__main__':
