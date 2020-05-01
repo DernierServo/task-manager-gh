d486f1c3ac8ee395a0b57a7f1687ebf1f2de9fe7
dservo <arrasco.cs@gmail.com>
2020-05-01 14:58:52 -0500

    Response method

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, request
-
+from flask import Flask, request, make_response, redirect
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
+def index():
 	user_ip = request.remote_addr
+	response = make_response(redirect('/hello'))
+	response.set_cookie('usr_ip', user_ip)
+
+	return response
+
+
+@app.route('/hello')
+def hello():
+	user_ip = request.cookies.get('user_ip')
+
 	return f'hello world from DServo labs, your IP is {user_ip}'
+
 
 if __name__ == '__main__':
 	hello()
