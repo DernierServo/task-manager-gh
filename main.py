e108e5f7f854acb3c0a7de04dc33c88b7db2b281
dservo <arrasco.cs@gmail.com>
2020-05-01 13:24:39 -0500

    Request method

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'hello world from Flask'
+	user_ip = request.remote_addr
+	return f'hello world from DServo labs, your IP is {user_ip}'
 
 if __name__ == '__main__':
 	hello()
