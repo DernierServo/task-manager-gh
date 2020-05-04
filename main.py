5a94e50080980778b2658099c69027549a4b18da
dservo <arrasco.cs@gmail.com>
2020-05-04 16:22:26 -0500

    Boostrap & Sessions

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+from uuid import uuid4
+
 
 app = Flask(__name__)
+secret_key = str(uuid4())
+app.config['SECRET_KEY'] = secret_key
+bootstrap = Bootstrap(app)
 todos = ['Read books', 'Cook the dinner', 'Study for an exam']
+
 
 @app.errorhandler(404)
 def not_found(error):
 	return render_template('404.html', error=error)
+
 
 @app.errorhandler(500)
 def server_error(error):
@@ -16,14 +24,13 @@ def server_error(error):
 def index():
 	user_ip = request.remote_addr
 	response = make_response(redirect('/hello'))
-	response.set_cookie('user_ip', user_ip)
-
+	session['user_ip'] = user_ip
 	return response
 
 
 @app.route('/hello')
 def hello():
-	user_ip = request.cookies.get('user_ip')
+	user_ip = session.get('user_ip')
 	context = {
 		'user_ip': user_ip,
 		'todos': todos,
