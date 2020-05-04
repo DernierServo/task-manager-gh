78dfae9d7fc8aa0796d792ba82cd4f2ce4c9791f
dservo <arrasco.cs@gmail.com>
2020-05-04 18:26:09 -0500

    Flask-WTF forms & POST methods

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from uuid import uuid4
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField	#Import fields
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
@@ -9,6 +12,11 @@ app.config['SECRET_KEY'] = secret_key
 bootstrap = Bootstrap(app)
 todos = ['Read books', 'Cook the dinner', 'Study for an exam']
 
+
+class LoginForm(FlaskForm):
+	username = StringField('User name', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Send')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -28,16 +36,27 @@ def index():
 	return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
 	user_ip = session.get('user_ip')
+	login_form = LoginForm()
+	username = session.get('username')
 	context = {
 		'user_ip': user_ip,
 		'todos': todos,
+		'login_form': login_form,
+		'username': username,
 	}
 
+	#Detecta cuando se envía un método Post y luego valida la forma
+	if login_form.validate_on_submit():
+		username = login_form.username.data
+		#guardando el username en la sesion:
+		session['username'] = username
+		return redirect(url_for('index'))
+
 	return render_template('hello.html', **context)
-	#return render_template('hello.html', user_ip = user_ip)
+
 
 
 if __name__ == '__main__':
